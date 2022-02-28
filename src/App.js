9f68bcf9ca6d74535dc218fa254fe774a7957ee4
meaganruan <meaganruan1214@gmail.com>
2022-02-27 21:13:16 -0500

    removed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import signUp from "./pages/signupPage";
-import logIn from "./pages/loginPage";
-import profile from "./pages/profile";
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    // <div className="App">
-    <Router>
+    <div className="App">
       <Navbar />
-      {/* switch turned into routes. */}
-      <Routes>
-        {/* maybe use element instead of component */}
-        <Route path="/loginPage" component={logIn} />
-        <Route path="/signupPage" component={signUp} />
-        <Route path="/profile" component={profile} />
-      </Routes>
-    </Router>
-    // </div>
+    </div>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,29 @@
 import React from "react";
 import "./navbar.css";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+// import styled from "styled-components";
 const Navbar = () => {
   return (
     <div>
       <div class="header">
         <ul class="nav-left">
           <li>
-            <Link to="home">
+            <a href="home">
               <img src="/eventlylogo.svg" alt="evently logo" />
-            </Link>
+            </a>
           </li>
           <li>Evently</li>
         </ul>
         <ul class="nav-right">
           <li>
-            <Link to="loginPage.js">Log In</Link>
+            <a href="loginPage.js">Log In</a>
           </li>
           <li>
-            <Link to="signupPage.js">Sign Up</Link>
+            <a href="signupPage.js">Sign Up</a>
           </li>
           <li>
-            <Link to="Profile.js">
+            <a href="Profile.js">
               <img src="/profilelogo.svg" alt="profile" />
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
