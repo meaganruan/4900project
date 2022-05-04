26da69756aed55899058f0554b79f252fb137042
meaganruan <meaganruan1214@gmail.com>
2022-05-04 18:48:32 -0400

    restructured

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
 
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./data/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./backend/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(8000, () => {
+  console.log('JSON Server is running')
+})
+
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./db.json');
+// const middlewares = jsonServer.defaults({
+//   static: './build'
+// });
+// const PORT = process.env.PORT || 8000;
+// server.use(middlewares);
+// server.use(jsonServer.rewriter({
+//   '/api/*': '/$1',
+// }))
+// server.use(router);
+// server.listen(PORT, () => {
+//   console.log('Server is running');
+// });
