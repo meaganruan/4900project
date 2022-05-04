fccdae0d118a8bfbc291f32659ee09cd0ac1d762
meaganruan <meaganruan1214@gmail.com>
2022-05-04 19:53:23 -0400

    removed procfile

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./backend/db.json')
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, './backend/db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log('JSON Server is running')
