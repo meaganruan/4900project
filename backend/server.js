747497ae8244bed228b5e2512c18e965849e238c
meaganruan <meaganruan1214@gmail.com>
2022-05-04 19:58:52 -0400

    changed db.json

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const path = require('path')
-const router = jsonServer.router(path.join(__dirname, './backend/db.json'))
+const router = jsonServer.router(path.join(__dirname, './db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
